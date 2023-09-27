4aa2c07f98c20bf77b9e240bb97897c28942948c
dorimir <144936659+dorimir@users.noreply.github.com>
2023-09-27 18:28:53 +0200

    V.1

---
+++ documento_1.py
@@ -0,0 +1,9 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
+print("Hello world :( ")
+=======
+print("Hello world")
+>>>>>>> main
+=======
+print("Hello world WORLD ")
+>>>>>>> 13564a230e9087adc13c170d879d4281d033896c
